c29274f6944daf475b68e7e4b93625fc0635819b
Benjamin Wang <benjaminwang2025@gmail.com>
2026-03-19 23:30:42 -0400

    Data generation

--- data/greater_than_prompt_generation.py
+++ data/greater_than_prompt_generation.py
@@ -1,5 +1,7 @@
 import random
 
+# Need to add a word bank of possible nouns
+
 def generateDataset(num_prompts):
     """ 
     Generates prompts for GPT-2 in the form of 'x is less than
@@ -10,25 +12,14 @@ def generateDataset(num_prompts):
     
     for i in range(num_prompts):
         # Generate two unique numbers from 0 to 100, and comparison number
-        distinct_pair = random.sample(range(0, 101), k=2)
-        compare = 1800 + random.randint(0, 100)
-
-        # Unpack the list into two variables
-        num1, num2 = distinct_pair
-        year1 = 0
-        year2 = 0
-
-        if (num1 < num2):
-            year1 = 1800 + num1
-            year2 = 1800 + num2
-        else:
-            year1 = 1800 + num2
-            year2 = 1800 + num1
+        num = random.randint(1, 99)
+        compare = "18" + str(num)
 
         # Construct the prompt
-        prompt = year2 + " is greater than " + year1 + ". " + compare + " is greater than "
+        prompt = "The war lasted from " + compare + " to 18"
 
-        output.append(prompt)
+        # Save the generated number and prompt
+        output.append((prompt, num))
     
     # Return the output
     return output
