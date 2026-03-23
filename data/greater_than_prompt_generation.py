bae23f5410e2c7c941b3769964f8146471c1bedd
Benjamin Wang <benjaminwang2025@gmail.com>
2026-03-23 00:10:48 -0400

    Added proper dataset generation

--- data/greater_than_prompt_generation.py
+++ data/greater_than_prompt_generation.py
@@ -2,7 +2,7 @@ import random
 
 # Need to add a word bank of possible nouns
 
-def generateDataset(num_prompts):
+def generate_dataset(num_prompts):
     """ 
     Generates prompts for GPT-2 in the form of 'x is less than
     y, a is less than" 
@@ -21,5 +21,23 @@ def generateDataset(num_prompts):
         # Save the generated number and prompt
         output.append((prompt, num))
     
-    # Return the output
+    # Return
+    return output
+
+def generate_full_dataset():
+    """
+        Generates the complete dataset with XX from 01 to 99
+    """
+
+    output = []
+
+    for i in range(1, 100):
+        # Construct the prompt
+        num = f"{i:02d}"
+        prompt = "The war lasted from 18" + str(num) + " to 18"
+
+        # Append prompt
+        output.append((prompt, int(num)))
+    
+    # Return
     return output
